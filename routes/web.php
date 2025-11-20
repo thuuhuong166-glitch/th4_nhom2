a8e4bd706dcffc38e38d3ddc4c8234d2cec94201
KieuDiem <hoangkieudiem2709@gmail.com>
2025-11-20 16:54:48 +0700

    Update web.php
    ui: Thiết kế layout Blade và định tuyến cho frontend và admin

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Front\PostController as FrontPostController;
-use App\Http\Controllers\Admin\PostController as AdminPostController;
 
-// Frontend
-Route::get('/',               [FrontPostController::class, 'index'])->name('home');
-Route::get('/posts',          [FrontPostController::class, 'index'])->name('front.posts.index');
-Route::get('/posts/{slug}',   [FrontPostController::class, 'show'])->name('front.posts.show');
-
-// Admin (demo không gắn auth, bạn có thể thêm middleware 'auth' sau)
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::resource('posts', AdminPostController::class)->except(['show']);
+Route::view('/', 'welcome'); // tạm thời, sẽ thay ở commit 3
+Route::prefix('admin')->group(function () {
+    Route::view('/posts', 'admin.stub'); // tạm, sẽ thay ở commit 5
 });
